353fdf4b2fb01fde901b11087182c1bf76883303
mdmonjumon <mdmonjumon786@gmail.com>
2026-02-23 16:19:41 +0600

    authProvider created

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,12 @@
+import AuthContext from "./AuthContext";
+
+const AuthProvider = ({ children }) => {
+  const authInfo = {
+    name: "monju",
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
